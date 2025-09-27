7a9f05ca28b580a9e73d6e49ea8593a83c066eac
pachamama-studios <jp.hurtado@pachamama-studios.com>
2025-09-27 17:33:04 +0200

    Update preload.js

--- core-app/main/preload.js
+++ core-app/main/preload.js
@@ -1,20 +1,72 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
-// Expose IPC functions to the renderer
+console.log('Preload script loaded');
+
+// Expose a limited API to the renderer
 contextBridge.exposeInMainWorld('electronAPI', {
   // Dataset functions
-  getDatasets: () => ipcRenderer.invoke('get-datasets'),
-  loadDataset: (datasetId) => ipcRenderer.invoke('load-dataset', datasetId),
+  getDatasets: () => {
+    console.log('getDatasets called from renderer');
+    return ipcRenderer.invoke('get-datasets');
+  },
   
-  // Event listeners
-  onSphereUpdate: (callback) => ipcRenderer.on('sphere-update', (event, data) => callback(data)),
-  onSliceUpdate: (callback) => ipcRenderer.on('slice-update', (event, data) => callback(data)),
-  onMediaControl: (callback) => ipcRenderer.on('media-control', (event, data) => callback(data)),
-  onLoadDataset: (callback) => ipcRenderer.on('load-dataset', (event, data) => callback(data)),
+  loadDataset: (datasetId) => {
+    console.log('loadDataset called from renderer with:', datasetId);
+    return ipcRenderer.invoke('load-dataset', datasetId);
+  },
+  
+  // Event listeners - now returns unsubscribe function
+  onSphereUpdate: (callback) => {
+    console.log('onSphereUpdate listener added');
+    const subscription = (event, data) => callback(data);
+    ipcRenderer.on('sphere-update', subscription);
+    return () => {
+      console.log('onSphereUpdate listener removed');
+      ipcRenderer.removeListener('sphere-update', subscription);
+    };
+  },
+  
+  onSliceUpdate: (callback) => {
+    console.log('onSliceUpdate listener added');
+    const subscription = (event, data) => callback(data);
+    ipcRenderer.on('slice-update', subscription);
+    return () => {
+      console.log('onSliceUpdate listener removed');
+      ipcRenderer.removeListener('slice-update', subscription);
+    };
+  },
+  
+  onMediaControl: (callback) => {
+    console.log('onMediaControl listener added');
+    const subscription = (event, data) => callback(data);
+    ipcRenderer.on('media-control', subscription);
+    return () => {
+      console.log('onMediaControl listener removed');
+      ipcRenderer.removeListener('media-control', subscription);
+    };
+  },
+  
+  onLoadDataset: (callback) => {
+    console.log('onLoadDataset listener added');
+    const subscription = (event, data) => callback(data);
+    ipcRenderer.on('load-dataset', subscription);
+    return () => {
+      console.log('onLoadDataset listener removed');
+      ipcRenderer.removeListener('load-dataset', subscription);
+    };
+  },
   
   // Send sphere updates to main process
-  sendSphereUpdate: (data) => ipcRenderer.send('sphere-update', data),
+  sendSphereUpdate: (data) => {
+    console.log('sendSphereUpdate called with:', data);
+    ipcRenderer.send('sphere-update', data);
+  },
   
-  // Remove all listeners
-  removeAllListeners: () => ipcRenderer.removeAllListeners()
+  // Send media controls
+  sendMediaControl: (data) => {
+    console.log('sendMediaControl called with:', data);
+    ipcRenderer.send('media-control', data);
+  }
 });
+
+console.log('electronAPI exposed');
